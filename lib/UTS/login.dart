b1dfc9cb08fea5218314875d6d7fa8ec5c6fa867
Reeptto <0204ariaprianto@gmail.com>
2025-10-23 20:17:56 +0700

    Masih Membuat Form

--- lib/UTS/login.dart
+++ lib/UTS/login.dart
@@ -10,48 +10,122 @@ class LoginPage extends StatelessWidget {
         width: double.infinity,
         child: Padding(
           padding: const EdgeInsets.only(top: 20, left: 70, right: 10.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
             children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('gambar/Logo.png'),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'LKS',
+                        style: TextStyle(
+                          fontSize: 46,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue
+                        ),
+                      ),
+                      Text(
+                        'MART',
+                        style: TextStyle(
+                          fontSize: 46,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                ],
+              ),
+            Container(
+              // margin: EdgeInsets.only(top: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("Enter your ID and Password to sign in!",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+              ),
+            ),
+            Container(
+              
+              child: SizedBox(
+                
+                height: 30,
+                child: Row(
+                  children: [
+                    Text("Email: "),
+                  ],
+                )
+              ),
+            ),
               Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('gambar/Logo.png'),
-                    fit: BoxFit.contain,
+                width: 400,
+                child: Container(
+                  child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
+                )
               ),
-              SizedBox(width: 16),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'LKS',
-                    style: TextStyle(
-                      fontSize: 46,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue
-                    ),
-                  ),
-                  Text(
-                    'MART',
-                    style: TextStyle(
-                      fontSize: 46,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue
-                    ),
-                  ),
-                  
+              SizedBox(
+              
+              height: 30,
+              child: Row(
+                children: [
+                  Text("Password: "),
                 ],
+              )
+            ),
+              Container(
+                width: 400,
+                child: Container(
+                  child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                )
               ),
             ],
           ),
         ),
+
       ),
     );
   }
