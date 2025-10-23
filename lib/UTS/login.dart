40481577cbb7629524530d093aacabffa22a365d
Reeptto <0204ariaprianto@gmail.com>
2025-10-23 19:27:32 +0700

    Membuat Judul form

--- lib/UTS/login.dart
+++ lib/UTS/login.dart
@@ -5,6 +5,44 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("gambar/Logo.png"))
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text("LKS", style: TextStyle(
+                        fontSize: 56,
+                        fontStyle: 
+                      ),),
+                    ),
+                    Container(
+                      child: Text("MART", style: TextStyle(
+                        fontSize: 56
+                      ),),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
