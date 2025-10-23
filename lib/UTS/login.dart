2e9f1436847300679d49e9cc961021d8a45a91f3
Reeptto <0204ariaprianto@gmail.com>
2025-10-23 20:00:49 +0700

    Mulai Membuat Form

--- lib/UTS/login.dart
+++ lib/UTS/login.dart
@@ -6,46 +6,51 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('gambar/Logo.png'),
-                  fit: BoxFit.contain,
+      body: Container(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20, left: 70, right: 10.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('gambar/Logo.png'),
+                    fit: BoxFit.contain,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 16),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'LKS',
-                  style: TextStyle(
-                    fontSize: 46,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue
+              SizedBox(width: 16),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'LKS',
+                    style: TextStyle(
+                      fontSize: 46,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue
+                    ),
                   ),
-                ),
-                Text(
-                  'MART',
-                  style: TextStyle(
-                    fontSize: 46,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue
+                  Text(
+                    'MART',
+                    style: TextStyle(
+                      fontSize: 46,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
